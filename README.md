1a81fb0271d9eac7834e5f24f62f5e3840be6ceb
Jason Kim <json@Jasons-MacBook-Pro.local>
2024-09-19 11:28:36 +0800

    Model과 db migration 완성.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Hello
+# Laravel 를  사용하여 Blog Web Site 만들기.
+1. 
---
+++ database/migrations/2024_09_19_030444_create_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // 사용자와의 외래 키 설정
+            $table->string('title');
+            $table->text('content');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
